7ffbf146417abba4c17527be3661b3dc51fc9e10
hoppafrost-glitch <hoppafrost@gmail.com>
2026-08-15 13:02:26 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,25 @@
+/* Tatame · funciona sin cobertura una vez instalada */
+const CACHE = "tatame-v2";
+const ARCHIVOS = ['./', './index.html', './manifest.json',
+  './icon-180.png', './icon-192.png', './icon-512.png', './icon-maskable.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ARCHIVOS)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys()
+    .then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k))))
+    .then(() => self.clients.claim()));
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then(hit => hit || fetch(e.request).then(res => {
+      const copia = res.clone();
+      caches.open(CACHE).then(c => c.put(e.request, copia)).catch(()=>{});
+      return res;
+    }).catch(() => caches.match('./index.html')))
+  );
+});
